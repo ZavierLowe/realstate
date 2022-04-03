531824deb2dd5c95bab3b130727fd694fecae20f
zavier <zavier.lowe@gmail.com>
2022-04-03 19:20:52 -0400

    card component almost
    fixed merge

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from 'next'
 import NavBar from '../components/NavBar'
+import ProductCard from '../components/ProductCard'
 
 
 const Home: NextPage = () => {
   return (
     <div>
       <NavBar/>
+      <ProductCard/>
+
     </div>
   )
 }
